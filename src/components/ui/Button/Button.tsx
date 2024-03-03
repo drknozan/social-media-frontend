a31031cf7886f5ecb67b81cb7daad7639a25e032
drknozan <drknozan@gmail.com>
2024-03-03 11:24:07 +0300

    feat: add button component

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,15 @@
+import { ButtonHTMLAttributes } from 'react';
+import styles from './Button.module.scss';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: 'sm' | 'md';
+  variant?: 'primary' | 'secondary';
+}
+
+export const Button = ({ size = 'md', variant = 'primary', ...props }: ButtonProps) => {
+  return (
+    <button className={`${styles.button} ${styles[variant]} ${styles[size]}`} {...props}>
+      {props.children}
+    </button>
+  );
+};
