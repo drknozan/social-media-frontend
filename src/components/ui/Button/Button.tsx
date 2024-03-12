6ea93e4aca8abe7f3fa0f63690f0fd69fa2bab6f
drknozan <drknozan@gmail.com>
2024-03-12 21:28:13 +0300

    feat: add loading state to button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,15 +1,27 @@
 import { ButtonHTMLAttributes } from 'react';
 import styles from './Button.module.scss';
+import { LoadingOutlined } from '@ant-design/icons';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   size?: 'sm' | 'md';
   variant?: 'primary' | 'secondary';
+  loading?: boolean;
 }
 
-const Button = ({ size = 'md', variant = 'primary', ...props }: ButtonProps) => {
+const Button = ({ size = 'md', variant = 'primary', loading, ...props }: ButtonProps) => {
   return (
-    <button className={`${styles.button} ${styles[variant]} ${styles[size]}`} {...props}>
-      {props.children}
+    <button
+      className={`${styles.button} ${styles[variant]} ${styles[size]} ${loading && styles.loading}`}
+      disabled={loading}
+      {...props}
+    >
+      {loading ? (
+        <>
+          Loading... <LoadingOutlined />
+        </>
+      ) : (
+        props.children
+      )}
     </button>
   );
 };
