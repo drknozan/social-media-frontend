883be570d02c469be6c09494312fd79e3f76a7a7
drknozan <drknozan@gmail.com>
2024-03-12 21:56:44 +0300

    feat: display data on post item

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -1,21 +1,31 @@
 import { PlusOutlined } from '@ant-design/icons';
-import Button from '@components/ui/Button';
+import Button from '@ui/Button';
 import styles from './PostItem.module.scss';
+import { Post } from '@src/types/Post';
+import { useNavigate } from 'react-router-dom';
 
-const PostItem = () => {
+const PostItem = ({ slug, title, content, createdAt, user, community }: Post) => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.communityName}></h1>
-      <h2 className={styles.title}></h2>
+      <h1 className={styles.communityName}>{community?.name}</h1>
+      <h2 className={styles.title} onClick={() => navigate(`/post/${slug}`)}>
+        {title}
+      </h2>
       <div className={styles.line}></div>
       <div className={styles.user}>
-        <div className={styles.username}></div>
+        <div className={styles.username} onClick={() => navigate(`/users/${user?.username}`)}>
+          {user?.username}
+        </div>
         <Button size="sm" variant="secondary">
           Follow <PlusOutlined />
         </Button>
       </div>
-      <p className={styles.content}></p>
-      <div className={styles.date}></div>
+      <p className={styles.content} onClick={() => navigate(`/post/${slug}`)}>
+        {content}
+      </p>
+      <div className={styles.date}>{createdAt?.toString()}</div>
     </div>
   );
 };
