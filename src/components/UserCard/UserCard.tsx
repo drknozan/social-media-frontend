d7fcbdc18602345a11b97d07971c69cf3e761eb7
drknozan <drknozan@gmail.com>
2024-03-13 14:03:01 +0300

    feat: display data on user card

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,26 +1,23 @@
+import { User } from '@src/types/User';
 import styles from './UserCard.module.scss';
 import Button from '@ui/Button';
 
-const UserCard = () => {
+const UserCard = ({ username, _count, bio }: User) => {
   return (
     <div className={styles.container}>
       <div className={styles.userInfo}>
-        <h1 className={styles.username}></h1>
+        <h1 className={styles.username}>{username}</h1>
         <div className={styles.line}></div>
-        <p className={styles.userBio}></p>
+        <p className={styles.userBio}>{bio}</p>
         <div className={styles.line}></div>
         <div className={styles.infoContainer}>
           <div className={styles.followerInfo}>
-            <span className={styles.followerCount}></span>
+            <span className={styles.followerCount}>{_count?.followers}</span>
             <span>Followers</span>
           </div>
           <div className={styles.followedInfo}>
-            <span className={styles.followedCount}></span>
+            <span className={styles.followedCount}>{_count?.followed}</span>
             <span>Followed</span>
-          </div>
-          <div className={styles.creationInfo}>
-            <span className={styles.creationDate}></span>
-            <span>Created at</span>
           </div>
         </div>
       </div>
